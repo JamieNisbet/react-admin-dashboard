2a6b5568b33fbf00d7115018969af95351bad415
Jamie.Nisbet <jamie.nisbet@apoyar.eu>
2022-10-22 02:17:05 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+import "./index.css";
+import App from "./App";
+import { ContextProvider } from "./context/ContextProvider";
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ContextProvider>
       <App />
-    </Provider>
-  </React.StrictMode>
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { BsCurrencyDollar } from "react-icons/bs";
+import { GoPrimitiveDot } from "react-icons/go";
+import { Stacked, Pie, Button, SparkLine } from "../components";
+import {
+  earningData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
+import { useStateContext } from "../context/ContextProvider";
+
+const Ecommerce = () => {
+  const { currentColor } = useStateContext();
+  return (
+    <div className="mt-24">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3">
+          <div className="flex justify-between items-center">
+            <div>
+              <p>Earnings</p>
+              <p className="text-2xl">$65,453.98</p>
+            </div>
+          </div>
+          <div className="mt-6">
+            <Button
+              color="white"
+              bgColor={currentColor}
+              text="Download"
+              borderRadius="10px"
+              size="md"
+            />
+          </div>
+        </div>
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark: dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl"
+            >
+              <button
+                className="text-2xl opacity-9 rounded-full p-4 hover:drop-shadow-xl"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+              >
+                {item.icon}
+              </button>
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm ${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-between">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div className="">
+                <p>
+                  <span className="text-3xl font-semibold">$94,454.73</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    34%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$94,454.73</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+              <div className="mt-5">
+                <SparkLine
+                  currentColor={currentColor}
+                  id="area-sparkLine"
+                  height="160px"
+                  type="Area"
+                  data={SparklineAreaData}
+                  width="320"
+                  color="rgb(242, 252, 253)"
+                />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor={currentColor}
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked width="275px" height="350px" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ecommerce;
